16b21b35200b31b931126b159684ab8c9e76aa3c
Adam Hopkins <adamhopkins87@gmail.com>
2020-01-10 10:39:24 +0000

    added first test with Preact testing library

---
+++ App.spec.js
@@ -0,0 +1,13 @@
+import { h } from 'preact';
+import { render, cleanup } from "@testing-library/preact";
+import "@testing-library/jest-dom";
+import App from './App';
+
+
+describe('@App', () => {
+  afterEach(cleanup);
+  it('renders the app', () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  })
+})
