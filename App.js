bbe2a6de335cff0e30f5da685a8a4e084a425a1d
Adam Hopkins <adamhopkins87@gmail.com>
2020-01-10 15:41:22 +0000

    added initial app

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import { h } from 'preact';
+import { useQuery, useMutation, gql } from '@apollo/client'
 
-const App = () => (
-  <div>
-    <h1>Hello from Preact</h1>
-    <h1>Hello from Preactv2</h1>
-  </div>
-)
+const query = gql`
+query {
+  books {
+    id
+    title
+  }
+}
+`;
+
+const mutation = gql`
+  mutation {
+    updateBookTitle(id: 1, title: "New title") {
+      id
+      title
+    }
+  }
+`;
+
+
+const App = () => {
+  const { error, loading, data } = useQuery(query)
+  const [update] = useMutation(mutation)
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return (
+    <div>
+      <h1>Hello from Preact</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <button onClick={() => {
+        update()
+      }}>Update</button>
+    </div>
+    )
+  }
 
 export default App
