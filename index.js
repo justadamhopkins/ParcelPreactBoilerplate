c559e93317c424c90eaa561cd8f7f4451e1dd197
Adam Hopkins <adamhopkins87@gmail.com>
2020-01-28 20:15:20 +0000

    removed prettier

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import { h, render } from "preact";
+import { h, render } from 'preact';
 import {
   ApolloClient,
   HttpLink,
   InMemoryCache,
-  ApolloProvider
-} from "@apollo/client";
-import App from "./App";
+  ApolloProvider,
+} from '@apollo/client';
+import App from './App';
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: process.env.GRAPHQLURL
-  })
+    uri: process.env.GRAPHQLURL,
+  }),
 });
 
 render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
