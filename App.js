72b7f180d567eee67b8cf2c01009f19d5ad13319
Adam Hopkins <adamhopkins87@gmail.com>
2020-02-13 21:10:30 +0000

    updated app based on code formatting settings

--- App.js
+++ App.js
@@ -2,43 +2,43 @@ import { h } from 'preact';
 import { useQuery, useMutation, gql } from '@apollo/client';
 
 const query = gql`
-  query {
-    books {
-      id
-      title
-    }
-  }
+	query {
+		books {
+			id
+			title
+		}
+	}
 `;
 
 const mutation = gql`
-  mutation {
-    updateBookTitle(id: 1, title: "New title") {
-      id
-      title
-    }
-  }
+	mutation {
+		updateBookTitle(id: 1, title: "New title") {
+			id
+			title
+		}
+	}
 `;
 
 const App = () => {
-  const { error, loading, data } = useQuery(query);
-  const [update] = useMutation(mutation);
+	const { error, loading, data } = useQuery(query);
+	const [update] = useMutation(mutation);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  return (
-    <div>
-      <h1>{`Hello from Preact and the environment is ${process.env.ENVIRONMENT}`}</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      <button
-        type="button"
-        onClick={() => {
-          update();
-        }}
-      >
-        Update
-      </button>
-    </div>
-  );
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :(</p>;
+	return (
+		<div>
+			<h1>{`Hello from Preact and the environment is ${process.env.ENVIRONMENT}`}</h1>
+			<pre>{JSON.stringify(data, null, 2)}</pre>
+			<button
+				type='button'
+				onClick={() => {
+					update();
+				}}
+			>
+				Update
+			</button>
+		</div>
+	);
 };
 
 export default App;
--- App.spec.js
+++ App.spec.js
@@ -1,13 +1,12 @@
 import { h } from 'preact';
-import { render, cleanup } from "@testing-library/preact";
-import "@testing-library/jest-dom";
+import { render, cleanup } from '@testing-library/preact';
+import '@testing-library/jest-dom';
 import App from './App';
 
-
 describe('@App', () => {
-  afterEach(cleanup);
-  it('renders the app', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
-  })
-})
+	afterEach(cleanup);
+	it('renders the app', () => {
+		const { asFragment } = render(<App />);
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
