3714416695564159270f76756e001ae555b3d7b1
Adam Hopkins <adamhopkins87@gmail.com>
2020-02-13 10:37:12 +0000

    updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,29 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
-  extends: ["standard-preact", "airbnb"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
-  settings: {
-    react: {
-      pragma: "h"
-    }
-  },
-  plugins: ["react"],
-  rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-one-expression-per-line": "off",
-    "react/jsx-filename-extension": [
-      "error",
-      {
-        extensions: [".js", ".jsx"]
-      }
-    ]
-  }
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	extends: ['standard-preact', 'airbnb'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	settings: {
+		react: {
+			pragma: 'h',
+		},
+	},
+	plugins: ['react'],
+	rules: {
+		'indent': [2, 'tab'],
+		'no-tabs': 0,
+	},
 };
