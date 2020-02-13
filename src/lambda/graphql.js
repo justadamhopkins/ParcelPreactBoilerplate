d60ca7371f562f7ebd71ad66e1f5e5ed76e664cf
Adam Hopkins <adamhopkins87@gmail.com>
2020-02-13 10:35:29 +0000

    formatted server code

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -4,51 +4,51 @@ const { ApolloServer, gql } = require('apollo-server-lambda');
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+	# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    id: ID
-    title: String
-    author: String
-  }
+	# This "Book" type defines the queryable fields for every book in our data source.
+	type Book {
+		id: ID
+		title: String
+		author: String
+	}
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
+	# The "Query" type is special: it lists all of the available queries that
+	# clients can execute, along with the return type for each. In this
+	# case, the "books" query returns an array of zero or more Books (defined above).
+	type Query {
+		books: [Book]
+	}
 
-  type Mutation {
-    updateBookTitle(id: ID, title: String): Book
-  }
+	type Mutation {
+		updateBookTitle(id: ID, title: String): Book
+	}
 `;
 
 const books = [
-  {
-    id: 1,
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    id: 2,
-    title: 'Jurassic Paaaaaaaaaaaark',
-    author: 'Michael Crichton',
-  },
+	{
+		id: 1,
+		title: 'Harry Potter and the Chamber of Secrets',
+		author: 'J.K. Rowling',
+	},
+	{
+		id: 2,
+		title: 'Jurassic Paaaaaaaaaaaark',
+		author: 'Michael Crichton',
+	},
 ];
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
-  Query: {
-    books: () => books,
-  },
-  Mutation: {
-    updateBookTitle(_, params) {
-      return params;
-    },
-  },
+	Query: {
+		books: () => books,
+	},
+	Mutation: {
+		updateBookTitle(_, params) {
+			return params;
+		},
+	},
 };
 
 // The ApolloServer constructor requires two parameters: your schema
@@ -58,8 +58,8 @@ const server = new ApolloServer({ typeDefs, resolvers });
 // The `listen` method launches a web server.
 
 exports.handler = server.createHandler({
-  cors: {
-    origin: '*',
-    credentials: true,
-  },
+	cors: {
+		origin: '*',
+		credentials: true,
+	},
 });
