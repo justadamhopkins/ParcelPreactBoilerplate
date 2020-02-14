f4dee89b547292e21e293bc06acb406f55931cc2
Adam Hopkins <adamhopkins87@gmail.com>
2020-02-14 21:00:57 +0000

    added tests for error and loading state apollo component

--- App.spec.js
+++ App.spec.js
@@ -11,7 +11,7 @@ describe('@App', () => {
 			},
 			result: {
 				data: {
-					books: [{ id: '1', title: 'HarryPotter' }],
+					books: [{ id: 1, title: 'HarryPotter' }],
 				},
 			},
 		},
@@ -30,4 +30,76 @@ describe('@App', () => {
 
 		expect(asFragment()).toMatchSnapshot();
 	});
+	it('renders the loading state', async () => {
+		const { getByText } = render(
+			<MockedProvider mocks={mocks} addTypename={false}>
+				<App />
+			</MockedProvider>,
+		);
+
+		const ele = getByText(/Loading.../i);
+
+		expect(ele).toBeTruthy();
+	});
+	it('renders the error state', async () => {
+		const errorMock = [
+			{
+				request: {
+					query: bookQuery,
+				},
+			},
+		];
+
+		const { getByText, debug } = render(
+			<MockedProvider mocks={errorMock} addTypename={false}>
+				<App />
+			</MockedProvider>,
+		);
+
+		await wait();
+		debug();
+		const ele = getByText(/Error/i);
+
+		expect(ele).toBeTruthy();
+	});
+	// xit('fires mutation', async () => {
+	// 	const mutationMock = [
+	// 		{
+	// 			request: {
+	// 				query: bookQuery,
+	// 			},
+	// 			result: {
+	// 				data: {
+	// 					books: [{ id: 1, title: 'HarryPotter' }],
+	// 				},
+	// 			},
+	// 		},
+	// 		{
+	// 			request: {
+	// 				query: bookMutation,
+	// 			},
+	// 			result: {
+	// 				data: {
+	// 					updateBookTitle: {
+	// 						id: 1,
+	// 						title: 'New title',
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	];
+
+	// 	const { getByText, debug } = render(
+	// 		<MockedProvider mocks={mutationMock} addTypename={false}>
+	// 			<App />
+	// 		</MockedProvider>,
+	// 	);
+
+	// 	await wait();
+	// 	fireEvent.click(getByText('Update'));
+	// 	await wait();
+
+	// 	debug();
+	// 	// expect(getByText('New title')).toBeTruthy();
+	// });
 });
