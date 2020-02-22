2d2fc75f170d62e17dcc0f9b75bb0c2d9127f180
Adam Hopkins <adamhopkins87@gmail.com>
2020-02-22 18:03:09 +0000

    added custom font

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 import {
-	ApolloClient,
-	HttpLink,
-	InMemoryCache,
-	ApolloProvider,
-} from '@apollo/client';
-import { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
-import App from './App';
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider
+} from '@apollo/client'
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+import App from './App'
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-`;
-
+  @import url('https://fonts.googleapis.com/css?family=Bungee&display=swap');
+  body {
+    font-family: 'Bungee', sans-serif;
+  }
+`
 const client = new ApolloClient({
-	cache: new InMemoryCache(),
-	link: new HttpLink({
-		uri: process.env.GRAPHQLURL,
-	}),
-});
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: process.env.GRAPHQLURL
+  })
+})
 
 render(
-	<ApolloProvider client={client}>
-		<GlobalStyle />
-		<App />
-	</ApolloProvider>,
-	document.getElementById('root'),
-);
+  <ApolloProvider client={client}>
+    <GlobalStyle />
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
