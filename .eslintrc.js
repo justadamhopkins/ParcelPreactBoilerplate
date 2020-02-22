e88b85239f15378ccf885dc45d8061cb4b58b28a
Adam Hopkins <adamhopkins87@gmail.com>
2020-02-22 18:03:18 +0000

    removed eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,14 +22,7 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
-    'no-tabs': 0,
-    indent: ['error', 'tab'],
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/jsx-indent': [
-      2,
-      'tab',
-      { checkAttributes: false, indentLogicalExpressions: true }
-    ],
     'react/jsx-indent-props': [2, 'tab'],
     'jsx-quotes': ['error', 'prefer-single'],
     'object-curly-newline': [
